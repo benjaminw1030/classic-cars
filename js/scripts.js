6339f976f3b2fafad30bbb7f20a5635477048af6
Ben Wilson <benjaminw1030@gmail.com>
2021-08-10 11:04:28 -0700

    create classic car page with jquery
    Co-authored-by: Anthony McRae <toddmac12@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $("#porche-click").click(function () {
+    $("#porche").fadeToggle();
+  });
+
+  $("#ferrari-click").click(function () {
+    $("#ferrari").fadeToggle();
+  });
+
+  $("#ferrari2-click").click(function () {
+    $("#ferrari2").fadeToggle();
+  });
+});
